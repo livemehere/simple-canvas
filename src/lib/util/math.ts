b2882c7f0caeda91afa93fe0772b446822f610d1
livemehere <rhdxoals@gmail.com>
2023-08-10 16:01:42 +0900

    refactor: randomBetween

--- src/lib/util/math.ts
+++ src/lib/util/math.ts
@@ -1,3 +1,7 @@
-export function randomBetween(min:number, max:number) {
+export function randomBetween(min:number, max:number, isInt:boolean = false) {
+    const randomValue  = Math.random() * (max - min + 1) + min;
+    if (isInt) {
+        return Math.floor(randomValue);
+    }
     return Math.random() * (max - min) + min;
 }
